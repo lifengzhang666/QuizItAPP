441588eccfacddd2b5692caff9ca2aa55bbd9f09
TPL <409353514@qq.com>
2018-01-16 23:31:08 +0800

    修改data

--- MyRegister.php
+++ MyRegister.php
@@ -6,7 +6,14 @@
  * Time: 下午8:15
  */
 header("Content-Type:text/html;charset=utf-8");
-echo '222';
+
+//TODO:获取传来的数据
+$user=$_GET["username"];
+$email=$_GET["email"];
+$pwd=$_GET["password"];
+
+echo '获取到数据:','username:',$user,'email:',$email,'password:',$pwd;
+
 $dsn="mysql:dbname=testschema;host=localhost";
 $db_user='root';
 $db_pass='890123asd';
@@ -18,10 +25,7 @@ try{
 }catch(PDOException $e){
     echo '数据库连接失败'.$e->getMessage();
 }
-//TODO:获取传来的数据
-$user=$_GET["username"];
-$pwd=$_GET["password"];
-$email=$_GET["email"];
+
 
 //TODO:先查询是否已经存在，存在返回0，不存在就写入数据返回1
 //查询user是否存在
