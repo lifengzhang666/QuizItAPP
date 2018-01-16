41b43951326d11764f6464fb27cac798741855b7
gjj <220731240@qq.com>
2018-01-16 22:25:44 +0800

    新加

---
+++ MyRegister.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TPL
+ * Date: 2017/12/26
+ * Time: 下午8:15
+ */
+header("Content-Type:text/html;charset=utf-8");
+echo '222';
+$dsn="mysql:dbname=testschema;host=localhost";
+$db_user='root';
+$db_pass='890123asd';
+try{
+    $pdo=new PDO($dsn,$db_user,$db_pass);
+	 //foreach ($dbh->query('SELECT * from mytable') as $row) {
+     //   print_r($row); //你可以用 echo($GLOBAL); 来看到这些值
+    //}
+}catch(PDOException $e){
+    echo '数据库连接失败'.$e->getMessage();
+}
+//TODO:获取传来的数据
+$user=$_GET["username"];
+$pwd=$_GET["password"];
+$email=$_GET["email"];
+
+//TODO:先查询是否已经存在，存在返回0，不存在就写入数据返回1
+//查询user是否存在
+$sql="select * from mytable WHERE username='$user'";
+$res=$pdo->query($sql);
+if($res->fetchColumn()){
+	//存在此user
+    echo 'no';
+    return 'no1';
+}else{
+	//不存在此user
+    echo 'yes';
+    return 'yes1';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
